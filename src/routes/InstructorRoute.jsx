1c7ad466defd9d2260c8624fb95b92fac3a90764
jahid morol <jahidmorol2@gmail.com>
2023-06-10 18:30:38 +0600

    added instructorRoute

---
+++ src/routes/InstructorRoute.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import useInstructor from "../hooks/useInstructor";
+import Loading from "../pages/Sheared/Loading/Loading";
+
+const InstructorRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const [isInstructor, isInsLoading] = useInstructor();
+  const location = useLocation();
+
+  if (loading || isInsLoading) {
+    return <Loading></Loading>;
+  }
+
+  if (user && isInstructor) {
+    return children;
+  }
+  return <Navigate to="/" state={{ from: location }} replace></Navigate>;
+};
+
+export default InstructorRoute;
